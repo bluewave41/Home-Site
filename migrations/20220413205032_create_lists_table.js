8027cf8acffe3285be50d26dc21c7459f25100b0
bluewave41 <megaman22342@live.ca>
2022-04-13 21:59:21 -0700

    Started work on calendar component

--- migrations/20220413205032_create_lists_table.js
+++ migrations/20220413205032_create_lists_table.js
@@ -6,7 +6,9 @@ exports.up = function(knex) {
 	return knex.schema.createTable('lists', function(table) {
 		table.increments('listId').unique().notNullable();
 		table.integer('ownedId').unsigned().references('userId').inTable('users').onDelete('cascade').notNullable();
-		table.string('uuid', 36).unique().notNullable();
+        table.string('uuid', 36).unique().notNullable();
+        //TODO: add dates to lists
+        //TODO: add costs to lists?
 	})
 };
 
--- pages/shopping.tsx
+++ pages/shopping.tsx
@@ -1,11 +1,15 @@
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Calendar from 'components/Calendar';
 
 const Shopping = (props) => {
     return (
         <Box>
             <Box>
                 <h1>Shopping</h1>
+                <h3>You currently have no lists.</h3>
+                <Button variant='contained'>Create List</Button>
+                <Calendar />
             </Box>
         </Box>
     )
